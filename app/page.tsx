0877a90b00154b15a1372d73d18a48393c465f07
AdityaSahu786 <sahuadi786183@gmail.com>
2024-11-12 20:32:16 +0530

    Adding a secondary Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-import { Button } from "@/components/ui/button";
+import MaxWidthWrapper from "@/components/common/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-      <div>Hello World</div>
+      <MaxWidthWrapper>
+         <HeroSection/> 
+      </MaxWidthWrapper>
   );
+}
+
+function HeroSection() {
+  return (
+    <div className="py-20 md:py-28 text-center">
+      <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">
+          Create Engaging Plans In Seconds.
+      </h1>
+      <p className="mt-6 text-lg text-muted-foreground max-w-3xl mx-auto ">
+        Empower your teaching with AI-generated lesson plan tailored to your
+        needs. Save time and inspire your students with creative, engaging
+        content.
+      </p>
+      <div className="mt-10 flex flex-col sm:flex-row gap-4 justify-center">
+        <Link href="/" className={buttonVariants()}>
+        Get Started For Free{" "} 
+        </Link>
+        <Link href="/pricing"
+         className={buttonVariants({
+          variant: "secondary",
+          
+         })}
+        > Pricing</Link>
+      </div>
+    </div>
+  )
 }
